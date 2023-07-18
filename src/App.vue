a4e3934b42dacab4df47bceb95dc6f36a574d2d5
Alisher Ruziyev <eclair.desertire@gmail.com>
2023-07-19 00:52:34 +0300

    added swiper, header and drafts of website

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <div class="container">
+          <Header>
+
+          </Header>
+          <swiper
+            :modules="modules"
+            :slides-per-view="1"
+            :space-between="50"
+            @swiper="onSwiper"
+            @slideChange="onSlideChange"
+            navigation
+            :pagination="{clickable: true}"
+
+          >
+            <swiper-slide>
+              <div class="testslider">dddd</div>
+            </swiper-slide>
+            <swiper-slide>
+              <div class="testslider">dsdad</div>
+            </swiper-slide>
+            <swiper-slide>
+              <div class="testslider">dsadsa</div>
+            </swiper-slide>
+
+            
+          </swiper>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper);
+      };
+      const onSlideChange = () => {
+        console.log('slide change');
+      };
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, Scrollbar, A11y],
+      };
+    },
 }
 </script>
 
 <style>
+body{
+  background-color: #1C1C1E;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +73,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container{
+  width: 86%;
+  margin: 0 auto;
+}
+.testslider{
+  width: 100%;
+  height: 800px;
+  background-color: blue;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <header class="Header">
+        <div class="logo">
+            <a href="#"><img :src="require('@/assets/images/firstlogo.png')" alt=""></a>
+        </div>
+        <nav>
+            <ul class="menu">
+                <li class="menu_link"><a href="#">Главная</a></li>
+                <li class="menu_link"><a href="#">Услуги</a></li>
+                <li class="menu_link"><a href="#">Работы</a></li>
+                <li class="menu_link"><a href="#">О нас</a></li>
+                <li class="menu_link"><a href="#">Связаться</a></li>
+            </ul>
+        </nav>
+        <div class="links">
+            <div class="social_links"></div>
+            <div class="social_links"></div>
+            <div class="social_links"></div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default{
+    name: "Header",
+    props:{
+
+    },
+    data(){
+        return{}
+    },
+    computed:{}
+}
+</script>
+
+<style>
+.Header{
+    display: flex;
+    justify-content: space-between;
+}
+ul{
+    display: flex;
+    justify-content: space-between;
+}
+.links{
+    display: flex;
+    justify-content: space-between;
+}
+.menu_link{
+    margin-left: 54px;
+}
+.social_links{
+    width: 70px;
+    height: 70px;
+    background-color: white;
+    margin-left: 11px;
+}
+</style>
